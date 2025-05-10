06b55475b441734307882ecdc0091c8a75666777
jefferson <jeffersonvitangcol@gmail.com>
2025-05-10 22:48:49 +0800

    feat: step up the login files

---
+++ frontend/src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign Up",
+};
+
+export default function Page() {
+  return (
+    <main>
+      <div>This sign up page</div>
+    </main>
+  );
+}
