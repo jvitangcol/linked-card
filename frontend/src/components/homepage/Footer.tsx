cd863bbf3acf751c25be52dfa5aa0da3963369be
jefferson <jeffersonvitangcol@gmail.com>
2025-05-16 09:30:13 +0800

    feat: finished the design mockup for footer

--- frontend/src/components/homepage/Footer.tsx
+++ frontend/src/components/homepage/Footer.tsx
@@ -1,22 +1,15 @@
 import Link from "next/link";
-import {
-  Facebook,
-  IdCard,
-  Instagram,
-  Linkedin,
-  Twitter,
-  Youtube,
-} from "lucide-react";
+import { Facebook, IdCard, Instagram, Linkedin, Twitter } from "lucide-react";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 
 export default function Footer() {
   return (
     <footer className="bg-secondary border-t-2 border-primary">
-      <div className="lg:container mx-auto px-5">
-        <div className="grid grid-cols-1 gap-8 py-10 border border-red-600">
-          {/* Logo and input email */}
-          <div className="border border-blue-600">
+      <div className="lg:container mx-auto px-5 space-y-7">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 py-10 ">
+          {/* ---------------------------- Logo and input email -------------------------------- */}
+          <div className="space-y-3 ">
             {/* Logo */}
             <Link
               href={"/"}
@@ -25,23 +18,32 @@ export default function Footer() {
               <span>Link</span>
               <IdCard size={28} className="text-primary" />
             </Link>
-            <div>
-              <form className="flex flex-col gap-2">
-                <Input type="email" placeholder="Your email here" />
-                <Button type="submit" className="w-full">
+
+            <p className="">
+              Subscribe to our newsletter for the latest updates on features and
+              releases.
+            </p>
+
+            <div className="space-y-2">
+              <form className="grid grid-cols-1 md:grid-cols-3 gap-2">
+                <Input
+                  type="email"
+                  placeholder="Your email here"
+                  className="md:col-span-2"
+                />
+                <Button type="submit" className="w-full md:w-[100px]">
                   Join
                 </Button>
               </form>
+              <p className="text-xs">
+                By subscribing, you consent to our Privacy Policy and agree to
+                receive updates.
+              </p>
             </div>
-
-            <p className="text-xs">
-              By subscribing, you consent to our Privacy Policy and agree to
-              receive updates.
-            </p>
           </div>
 
           {/* --------------------------- Links ----------------------------------- */}
-          <div className="grid grid-cols-1 gap-8 border border-yellow-600">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 ">
             {/* Quick links */}
             <div className="space-y-4">
               <h4 className="font-semibold">Quick Links</h4>
@@ -101,12 +103,6 @@ export default function Footer() {
                 >
                   LinkedIn Page
                 </Link>
-                <Link
-                  href={"/"}
-                  className="hover:underline hover:text-secondary-foreground transition delay-75 duration-200 ease-initial"
-                >
-                  YouTube Channel
-                </Link>
               </div>
             </div>
 
@@ -153,27 +149,20 @@ export default function Footer() {
                     LinkedIn
                   </Link>
                 </div>
-
-                <div className="flex gap-2">
-                  <Youtube size={20} />
-                  <Link
-                    href={"/"}
-                    className="hover:underline hover:text-secondary-foreground transition delay-75 duration-200 ease-initial"
-                  >
-                    YouTube
-                  </Link>
-                </div>
               </div>
             </div>
           </div>
         </div>
-        <div>
-          {/* Credits */}
-          <div>
-            {/* All rights */}
-            <h6>© 2025 LinkCard. All rights reserved.</h6>
 
-            <div>
+        {/* --------------------------- Credits ----------------------------------- */}
+        <div className="pb-10">
+          <div className="flex flex-col md:justify-between md:flex-row gap-4 text-sm">
+            {/* All rights */}
+            <h6 className="order-2 md:order-1 ">
+              © 2025 LinkCard. All rights reserved.
+            </h6>
+
+            <div className="flex flex-col md:flex-row gap-2 order-1 md:order-2 underline">
               <span>Privacy Policy</span>
               <span>Terms of Use</span>
               <span>Cookie Settings</span>
